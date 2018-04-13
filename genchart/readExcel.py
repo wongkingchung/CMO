3f519dd7dd37a64470dc27c2cfe00a78c1785a30
CONVOY\kcwong <kc.wong@convoy.com.hk>
2018-04-13 19:59:15 +0800

    estimate line

--- genchart/readExcel.py
+++ genchart/readExcel.py
@@ -12,8 +12,10 @@ actual = []
 actualdate = []
 d = 0
 date_format = "%Y-%m-%d %h:%m:%s"
+scan = True
+row=0
 
-for row in range(0, 15):
+while (scan):
     if row == 0 :
         project = df.loc[row,0]
     if row == 1 :
@@ -29,23 +31,29 @@ for row in range(0, 15):
             nxtday = fd + timedelta(days=7*d)
             actualdate.append(nxtday)
             d = d + 1
+        else:
+            scan = False
 
-    print df.loc[row,0]
+    row = row + 1
+    
+#    print df.loc[row,0]
     
 print project, fd, td
 print actual 
 print actualdate
+print wks
 
-actdict = {'actual':actual, 'date':actualdate}
-actdf = pandas.DataFrame(actdict)
-#actdf = pandas.DataFrame(actualdate, columns=['date'])
-
-print actdf
+rate = int( (100.0/wks))
+burndown = [rate * x for x in range(wks+1)]
+estimate = pandas.DataFrame(burndown, columns=['estimated'])
+estimate['date']= pandas.date_range(fd,td,freq='7D')
+print estimate
 
 plt.xticks(rotation=30)
 plt.title('Project 1')
 plt.ylabel('Completion %')
 #plt.plot( 'date','actual',data=actdf, label='Actual')
+plt.plot( 'date', 'estimated', data=estimate, label='Estimated')
 plt.plot( actualdate, actual, label='Actual')
 plt.legend()
 plt.show()
