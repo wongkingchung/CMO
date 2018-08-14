4ee8973548001677f7422cad8ddde968fcd1c76f
CONVOY\kcwong <kc.wong@convoy.com.hk>
2018-08-14 16:32:01 +0800

    fix the Chinese characters

--- ETL/IMPORT.py
+++ ETL/IMPORT.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import codecs
 import pyodbc
 import openpyxl
@@ -21,7 +22,7 @@ logpath = config.get('PROMOTION','logpath')
 
 
 driver= '{ODBC Driver 13 for SQL Server}'
-cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password+';Charset=UTF8')
 cursor = cnxn.cursor()
 
 
@@ -32,7 +33,7 @@ ended = False
 r=1
 c=0
 
-SQLCommand = ("DELETE icompare.imp_promotion;")
+SQLCommand = ("DELETE icompare_uat.imp_promotion;")
 cursor.execute(SQLCommand)
 
 
@@ -54,9 +55,6 @@ while not ended:
     if active == None:
         active = 1
 
-    if language == None:
-        language = 'en' 
-
     if planid == None and promotion_text == None and  startdate == None:
         ended = True
     else:
@@ -66,9 +64,16 @@ while not ended:
         if promotion_link != "":
             promotion_text = promotion_text + '<a href="'+promotion_link+'"> (click here)</a>' 
 
-        SQLCommand = ("INSERT INTO icompare.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",'" + promotion_text+"','"+language+"','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');")
+        if language == None:
+            SQLCommand = u"INSERT INTO icompare_uat.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','en','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
+            cursor.execute(SQLCommand)
+            SQLCommand = u"INSERT INTO icompare_uat.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','zh','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
+            cursor.execute(SQLCommand)
+        else:
+            SQLCommand = u"INSERT INTO icompare_uat.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','"+language+"','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
+            cursor.execute(SQLCommand)
+
         print SQLCommand
-        cursor.execute(SQLCommand)
 
 
 ws = wb['BV Promotion']
@@ -85,7 +90,6 @@ while not ended:
         promotion_link = ws.cell(row=r, column=4).hyperlink.target
 
     active = ws.cell(row=r, column=5).value
-    language = 'en'
     startdate = ws.cell(row=r, column=6).value
     enddate = ws.cell(row=r, column=7).value
 
@@ -94,9 +98,6 @@ while not ended:
 
     if active == None:
         active = 1
-
-    if language == None:
-        language = 'en' 
 
     if planid == None and promotion_text == None and  startdate == None:
         ended = True
@@ -107,22 +108,25 @@ while not ended:
         if promotion_link != "":
             promotion_text = promotion_text + '<a href="'+promotion_link+'"> (click here)</a>' 
 
-        SQLCommand = ("INSERT INTO icompare.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",'" + promotion_text+"','"+language+"','bv',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');")
+        SQLCommand = u"INSERT INTO icompare_uat.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','en','bv',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
+        print SQLCommand
+        cursor.execute(SQLCommand)
+        SQLCommand = u"INSERT INTO icompare_uat.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','zh','bv',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
         print SQLCommand
         cursor.execute(SQLCommand)
 
 ## insert new promotions
-SQLCommand = ("insert into icompare.imp_convoy_feature  select plan_id, 'promotion' as feature_name, stuff((select '<br><br>' +  (promotion) from icompare.imp_promotion where plan_id = a.plan_id and language = a.language and active = a.active FOR XML PATH(''),type).value('(./text())[1]','varchar(max)'),1,8,'') as promotion, 'D' as display_type, 'text' as data_type, active, getdate(), language, min(startdate) as startdate , max(enddate) as enddate from icompare.imp_promotion a where getdate()>=startdate and getdate() <=enddate and active = 1 and not exists (select 1 from icompare.imp_convoy_feature f where f.plan_id = a.plan_id and f.language = a.language ) group by plan_id, language,active")
+SQLCommand = u"insert into icompare_uat.convoy_feature  select plan_id, 'promotion' as feature_name, stuff((select '<br><br>' +  (promotion) from icompare_uat.imp_promotion where plan_id = a.plan_id and language = a.language and active = a.active order by type desc FOR XML PATH(''),type).value('(./text())[1]','nvarchar(max)'),1,8,'') as promotion, 'D' as display_type, 'text' as data_type, active, getdate(), language, min(startdate) as startdate , max(enddate) as enddate from icompare_uat.imp_promotion a where getdate()>=startdate and getdate() <=enddate and active = 1 and not exists (select 1 from icompare_uat.convoy_feature f where f.plan_id = a.plan_id and f.language = a.language ) group by plan_id, language,active"
 print SQLCommand
 cursor.execute(SQLCommand)
 
 ## inactivate promotions
-SQLCommand = ("update icompare.imp_convoy_feature set is_active = 0 from ( select * from icompare.imp_promotion where active = 0 or not (getdate() >= startdate and getdate() <= enddate)) ip where icompare.imp_convoy_feature.plan_id = ip.plan_id  and icompare.imp_convoy_feature.feature_name ='promotion'")
+SQLCommand = u"update icompare_uat.convoy_feature set is_active = 0 from ( select * from icompare_uat.imp_promotion where active = 0 or not (getdate() >= startdate and getdate() <= enddate)) ip where icompare_uat.convoy_feature.plan_id = ip.plan_id  and icompare_uat.convoy_feature.feature_name ='promotion'"
 print SQLCommand
 cursor.execute(SQLCommand)
 
 ## update promotions
-SQLCommand = ("update icompare.imp_convoy_feature set feature_value = ip.promotion, display_start_date = ip.startdate, display_end_date = ip.enddate, is_active=1 from (select plan_id, stuff((select '<br><br>' +  (promotion) from icompare.imp_promotion   where plan_id = a.plan_id   and language = a.language   and active = a.active  and getdate()>=startdate and getdate() <=enddate and active = 1 FOR XML PATH(''),type).value('(./text())[1]','varchar(max)'),1,8,'') as promotion, language,  min(startdate) as startdate , max(enddate) as enddate from icompare.imp_promotion a where getdate()>=startdate and getdate() <=enddate and active = 1 group by plan_id, language,active ) ip where icompare.imp_convoy_feature.plan_id = ip.plan_id and icompare.imp_convoy_feature.language = ip.language and icompare.imp_convoy_feature.feature_name = 'promotion'")
+SQLCommand = u"update icompare_uat.convoy_feature set feature_value = ip.promotion, display_start_date = ip.startdate, display_end_date = ip.enddate, is_active=1 from (select plan_id, stuff((select '<br><br>' +  (promotion) from icompare_uat.imp_promotion   where plan_id = a.plan_id   and language = a.language   and active = a.active  and getdate()>=startdate and getdate() <=enddate and active = 1 order by type desc FOR XML PATH(''),type).value('(./text())[1]','nvarchar(max)'),1,8,'') as promotion, language,  min(startdate) as startdate , max(enddate) as enddate from icompare_uat.imp_promotion a where getdate()>=startdate and getdate() <=enddate and active = 1 group by plan_id, language,active ) ip where icompare_uat.convoy_feature.plan_id = ip.plan_id and icompare_uat.convoy_feature.language = ip.language and icompare_uat.convoy_feature.feature_name = 'promotion'"
 print SQLCommand
 cursor.execute(SQLCommand)
 
