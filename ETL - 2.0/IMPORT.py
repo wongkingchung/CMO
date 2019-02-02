8f8f6180cde080a9f8ae01cb164cc14069f7e928
CONVOY\kcwong <kc.wong@convoy.com.hk>
2019-02-02 21:53:28 +0800

    added the sendemail module to send email after import

--- ETL - 2.0/IMPORT.py
+++ ETL - 2.0/IMPORT.py
@@ -8,7 +8,7 @@ import os
 import glob
 import shutil
 import datetime
-
+from sendemail import sendmail
 
 config = ConfigParser.ConfigParser()
 config.read('configloc.ini')
@@ -21,6 +21,7 @@ server = config.get('DB','server')
 database = config.get('DB','database')
 username = config.get('DB','username')
 password = config.get('DB','password')
+schema = config.get('DB','schema')
 
 promofilepath = config.get('PROMOTION','file')
 logpath = config.get('PROMOTION','logpath')
@@ -48,7 +49,7 @@ ended = False
 r=1
 c=0
 
-SQLCommand = ("DELETE icomparev2.imp_promotion;")
+SQLCommand = ("DELETE "+schema+".imp_promotion;")
 cursor.execute(SQLCommand)
 
 
@@ -80,12 +81,12 @@ while not ended:
             promotion_text = promotion_text + '<a href="'+promotion_link+'"> (click here)</a>' 
 
         if language == None:
-            SQLCommand = u"INSERT INTO icomparev2.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','en','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
+            SQLCommand = u"INSERT INTO "+schema+".imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','en','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
             cursor.execute(SQLCommand)
-            SQLCommand = u"INSERT INTO icomparev2.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','zh','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
+            SQLCommand = u"INSERT INTO "+schema+".imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','zh','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
             cursor.execute(SQLCommand)
         else:
-            SQLCommand = u"INSERT INTO icomparev2.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','"+language+"','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
+            SQLCommand = u"INSERT INTO "+schema+".imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','"+language+"','cp',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
             cursor.execute(SQLCommand)
 
         print SQLCommand
@@ -126,26 +127,26 @@ while not ended:
         if promotion_link != "":
             promotion_text = promotion_text + '<a href="'+promotion_link+'"> (click here)</a>' 
 
-        SQLCommand = u"INSERT INTO icomparev2.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','en','bv',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
+        SQLCommand = u"INSERT INTO "+schema+".imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','en','bv',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
         print SQLCommand
         cursor.execute(SQLCommand)
-        SQLCommand = u"INSERT INTO icomparev2.imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','zh','bv',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
+        SQLCommand = u"INSERT INTO "+schema+".imp_promotion (plan_id, promotion, language, type, active, startdate, enddate) VALUES (" +str(planid)+",N'" + promotion_text+"','zh','bv',"+str(active)+",'"+str(startdate)+"','"+str(enddate)+"');"
         print SQLCommand
         cursor.execute(SQLCommand)
 
 ## insert new promotions
 #SQLCommand = u"insert into icomparev2.convoy_feature  select plan_id, case when type='cp' then 'promotion' else 'bv_promotion' end as feature_name, stuff((select '<br><br>' +  (promotion) from icomparev2.imp_promotion where plan_id = a.plan_id and language = a.language and active = a.active order by type desc FOR XML PATH(''),type).value('(./text())[1]','nvarchar(max)'),1,8,'') as promotion, 'D' as display_type, 'text' as data_type, active, getdate(), language, min(startdate) as startdate , max(enddate) as enddate from icomparev2.imp_promotion a where getdate()>=startdate and getdate() <=enddate and active = 1 and not exists (select 1 from icomparev2.convoy_feature f where f.plan_id = a.plan_id and f.language = a.language ) group by plan_id, language,active, type"
-SQLCommand = u"insert into icomparev2.convoy_feature  select plan_id, case when type='cp' then 'promotion' else 'bv_promotion' end as feature_name, stuff((select '<br><br>' +  (promotion) from icomparev2.imp_promotion where plan_id = a.plan_id and language = a.language and type = a.type and active = a.active order by type desc FOR XML PATH(''),type).value('(./text())[1]','nvarchar(max)'),1,8,'') as promotion, 'D' as display_type, 'text' as data_type, active, getdate(), language, min(startdate) as startdate , max(enddate) as enddate from icomparev2.imp_promotion a where getdate()>=startdate and getdate() <=enddate and active = 1 and not exists (select 1 from icomparev2.convoy_feature f where f.plan_id = a.plan_id and f.language = a.language and f.feature_name = case when a.type ='cp' then 'promotion' else 'bv_promotion' end ) group by plan_id, language,active, type"
+SQLCommand = u"insert into "+schema+".convoy_feature  select plan_id, case when type='cp' then 'promotion' else 'bv_promotion' end as feature_name, stuff((select '<br><br>' +  (promotion) from "+schema+".imp_promotion where plan_id = a.plan_id and language = a.language and type = a.type and active = a.active order by type desc FOR XML PATH(''),type).value('(./text())[1]','nvarchar(max)'),1,8,'') as promotion, 'D' as display_type, 'text' as data_type, active, getdate(), language, min(startdate) as startdate , max(enddate) as enddate from "+schema+".imp_promotion a where getdate()>=startdate and getdate() <=enddate and active = 1 and not exists (select 1 from "+schema+".convoy_feature f where f.plan_id = a.plan_id and f.language = a.language and f.feature_name = case when a.type ='cp' then 'promotion' else 'bv_promotion' end ) group by plan_id, language,active, type"
 print SQLCommand
 cursor.execute(SQLCommand)
 
 ## inactivate promotions
-SQLCommand = u"update icomparev2.convoy_feature set is_active = 0 , display_start_date = ip.startdate, display_end_date = ip.enddate from ( select * from icomparev2.imp_promotion where active = 0 or not (getdate() >= startdate and getdate() <= enddate)) ip where icomparev2.convoy_feature.plan_id = ip.plan_id  and icomparev2.convoy_feature.feature_name = case when ip.type = 'cp' then 'promotion' else 'bv_promotion' end"
+SQLCommand = u"update "+schema+".convoy_feature set is_active = 0 , display_start_date = ip.startdate, display_end_date = ip.enddate from ( select * from "+schema+".imp_promotion where active = 0 or not (getdate() >= startdate and getdate() <= enddate)) ip where "+schema+".convoy_feature.plan_id = ip.plan_id  and "+schema+".convoy_feature.feature_name = case when ip.type = 'cp' then 'promotion' else 'bv_promotion' end"
 print SQLCommand
 cursor.execute(SQLCommand)
 
 ## update promotions
-SQLCommand = u"update icomparev2.convoy_feature set feature_value = ip.promotion, display_start_date = ip.startdate, display_end_date = ip.enddate, is_active=1 from (select plan_id, type, stuff((select '<br><br>' +  (promotion) from icomparev2.imp_promotion   where plan_id = a.plan_id   and language = a.language   and active = a.active and type = a.type and getdate()>=startdate and getdate() <=enddate and active = 1 order by type desc FOR XML PATH(''),type).value('(./text())[1]','nvarchar(max)'),1,8,'') as promotion, language,  min(startdate) as startdate , max(enddate) as enddate from icomparev2.imp_promotion a where getdate()>=startdate and getdate() <=enddate and active = 1 group by plan_id, language,active, type ) ip where icomparev2.convoy_feature.plan_id = ip.plan_id and icomparev2.convoy_feature.language = ip.language and icomparev2.convoy_feature.feature_name = case when ip.type = 'cp' then 'promotion' else 'bv_promotion' end"
+SQLCommand = u"update "+schema+".convoy_feature set feature_value = ip.promotion, display_start_date = ip.startdate, display_end_date = ip.enddate, is_active=1 from (select plan_id, type, stuff((select '<br><br>' +  (promotion) from "+schema+".imp_promotion   where plan_id = a.plan_id   and language = a.language   and active = a.active and type = a.type and getdate()>=startdate and getdate() <=enddate and active = 1 order by type desc FOR XML PATH(''),type).value('(./text())[1]','nvarchar(max)'),1,8,'') as promotion, language,  min(startdate) as startdate , max(enddate) as enddate from "+schema+".imp_promotion a where getdate()>=startdate and getdate() <=enddate and active = 1 group by plan_id, language,active, type ) ip where "+schema+".convoy_feature.plan_id = ip.plan_id and "+schema+".convoy_feature.language = ip.language and "+schema+".convoy_feature.feature_name = case when ip.type = 'cp' then 'promotion' else 'bv_promotion' end"
 print SQLCommand
 cursor.execute(SQLCommand)
 
@@ -157,3 +158,5 @@ if logpath[-1] != '\\':
 logfile = logpath + 'log_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.xlsx'
 #shutil.copy(promofile, logfile)
 shutil.move(promofile, logfile)
+
+sendmail('The promotion file '+promofile+ ' has been uploaded.')
